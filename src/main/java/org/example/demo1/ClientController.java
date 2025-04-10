2fbcdfa1912efec699278bd59265746477727bd5
udarasan <udarassanjeewa@gmail.com>
2025-04-10 11:13:55 +0530

    update content

--- src/main/java/org/example/demo1/ClientController.java
+++ src/main/java/org/example/demo1/ClientController.java
@@ -5,6 +5,11 @@ import javafx.fxml.FXML;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+
 public class ClientController {
 
     @FXML
@@ -13,8 +18,31 @@ public class ClientController {
     @FXML
     private TextField txtMessage;
 
+    Socket socket;
+    DataInputStream dataInputStream;
+    DataOutputStream dataOutputStream;
+    String message="";
+
+    public void initialize() {
+        new Thread(() -> {
+            try {
+                socket = new Socket("localhost",4000);
+                dataInputStream = new DataInputStream(socket.getInputStream());
+                while (true){
+                    message=dataInputStream.readUTF();
+                    txtArea.appendText("Server :"+message+"\n");
+                }
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }).start();
+    }
+
     @FXML
-    void sendOnAction(ActionEvent event) {
+    void sendOnAction(ActionEvent event) throws IOException {
+        dataOutputStream=new DataOutputStream(socket.getOutputStream());
+        dataOutputStream.writeUTF(txtMessage.getText());
+        dataOutputStream.flush();
 
     }
 
--- src/main/java/org/example/demo1/ServerController.java
+++ src/main/java/org/example/demo1/ServerController.java
@@ -5,6 +5,13 @@ import javafx.fxml.FXML;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class ServerController {
 
     @FXML
@@ -13,9 +20,34 @@ public class ServerController {
     @FXML
     private TextField txtMessage;
 
-    @FXML
-    void sendOnAction(ActionEvent event) {
+    ServerSocket serverSocket;
+    Socket socket;
+    DataOutputStream dataOutputStream;
+    DataInputStream dataInputStream;
+    String message="";
+    public void initialize() {
+       new Thread(() -> {
+           try {
+               serverSocket = new ServerSocket(4000);
+               txtArea.appendText("Server started\n");
+               socket = serverSocket.accept();
+               txtArea.appendText("Client connected\n");
+               dataInputStream = new DataInputStream(socket.getInputStream());
+               while (true) {
+                   message=dataInputStream.readUTF();
+                   txtArea.appendText("Client :"+message+"\n");
+               }
+           } catch (IOException e) {
+               throw new RuntimeException(e);
+           }
+       }).start();
+    }
 
+    @FXML
+    void sendOnAction(ActionEvent event) throws IOException {
+        dataOutputStream = new DataOutputStream(socket.getOutputStream());
+        dataOutputStream.writeUTF(txtMessage.getText());
+        dataOutputStream.flush();
     }
 
 }
